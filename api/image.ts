b4e021b3c1940cf131f09bbbe365595d51d90447
dtinth on Cloud9 <dtinth@spacet.me>
2020-07-10 15:10:59 +0000

    Add some actual routing!

--- api/image.ts
+++ api/image.ts
@@ -1,7 +1,7 @@
 import { NowRequest, NowResponse } from '@vercel/node'
-import { hello } from '../lib/dtinth'
+import { renderImage } from '../images'
 
 export default function (req: NowRequest, res: NowResponse) {
   res.setHeader('Content-Type', 'image/svg+xml')
-  res.send(hello())
+  res.send(renderImage(String(req.query.name)))
 }
