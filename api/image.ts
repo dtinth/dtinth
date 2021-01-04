f248432b93556799137ad76bcf57496d0face972
Thai Pangsakulyanont <dtinth@spacet.me>
2021-01-04 20:04:37 +0700

    svr

--- api/image.ts
+++ api/image.ts
@@ -5,6 +5,7 @@ export default async function (req: NowRequest, res: NowResponse) {
   try {
     const result = await renderImage(String(req.query.name))
     res.setHeader('Content-Type', 'image/svg+xml')
+    res.setHeader('Cache-Control', 's-maxage=1, stale-while-revalidate')
     res.send(result)
   } catch (error) {
     res.send('Error')
